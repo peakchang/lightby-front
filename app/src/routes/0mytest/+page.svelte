fd625b42f12e4f986e7e6074465bb9484d000c4c
rider <ridebus82@nate.com>
2025-07-13 13:54:11 +0900

    250713 asdfljoggoogogo

--- app/src/routes/0mytest/+page.svelte
+++ app/src/routes/0mytest/+page.svelte
@@ -1,1 +1,16 @@
-테스트인데 맞아?!?!?
+<script>
+    import { browser } from "$app/environment";
+    import { onMount } from "svelte";
+
+    function ssibalTest() {
+        window.open(`/testpop`, "popup", "width=550,height=670");
+    }
+
+    function paymentSuccess(e) {
+        console.log(e.data);
+    }
+</script>
+
+<svelte:window on:message={paymentSuccess} />
+
+<button on:click={ssibalTest}> testBtn </button>
