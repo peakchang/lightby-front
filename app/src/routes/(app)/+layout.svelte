bfb3ad69b8f38e75a123888a6e18a647e8191983
rider <ridebus82@nate.com>
2025-09-08 12:47:50 +0900

    250908 asfljogogogo

--- app/src/routes/(app)/+layout.svelte
+++ app/src/routes/(app)/+layout.svelte
@@ -75,6 +75,44 @@
     ></script>
 
     <script src="https://developers.kakao.com/sdk/js/kakao.js"></script>
+
+    <!-- 메타 스크립트 -->
+    <script>
+        !(function (f, b, e, v, n, t, s) {
+            if (f.fbq) return;
+            n = f.fbq = function () {
+                n.callMethod
+                    ? n.callMethod.apply(n, arguments)
+                    : n.queue.push(arguments);
+            };
+            if (!f._fbq) f._fbq = n;
+            n.push = n;
+            n.loaded = !0;
+            n.version = "2.0";
+            n.queue = [];
+            t = b.createElement(e);
+            t.async = !0;
+            t.src = v;
+            s = b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t, s);
+        })(
+            window,
+            document,
+            "script",
+            "https://connect.facebook.net/en_US/fbevents.js",
+        );
+        fbq("init", "1291886845819358");
+        fbq("track", "PageView");
+    </script>
+
+    <noscript
+        ><img
+            height="1"
+            width="1"
+            style="display:none"
+            src="https://www.facebook.com/tr?id=1291886845819358&ev=PageView&noscript=1"
+        />
+    </noscript>
 </svelte:head>
 
 <CustomModal bind:visible={loginAlertModalShow} closeBtn={false}>
