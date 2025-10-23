f5abddd9a7e5fbe69f76ad20316b0822b8fc854f
rider <ridebus82@nate.com>
2025-10-23 10:27:54 +0900

    251023 sadfljogogogogo

--- app/src/routes/+layout.server.js
+++ app/src/routes/+layout.server.js
@@ -1,7 +1,7 @@
 import { back_api, back_api_origin } from '$lib/const.js';
 import moment from 'moment-timezone';
 import { sql_con } from "$lib/server/db";
-import { getRandomBetween } from '$lib/lib.js';
+
 import axios from 'axios';
 
 
@@ -11,18 +11,11 @@ import axios from 'axios';
 // hooks.server.js 에서 받아온 유저 정보 +layout.js 로 넘기기 위한 중간 단계
 export const load = async ({ locals, request, cookies }) => {
 
-
     const visitCookie = cookies.get('visit', { path: '/' })
-
-
-    
-
 
     // 쿠키 설정이 안되어 있으면 작업 GO!!
     if (!visitCookie || visitCookie != 'true') {
-
         console.log('기존 DB 없으면 insert! 있으면 update! 가짜 카운트랑 진짜 카운트!');
-
         try {
             const res = await axios.get(`${back_api}/insert_n_update_count`)
         } catch (error) {
