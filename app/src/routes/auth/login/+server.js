50051173850739dbfa1113bc2f18a07eccde4470
rider <ridebus82@nate.com>
2025-10-23 10:36:18 +0900

    251023 asdfliogogogogo

--- app/src/routes/auth/login/+server.js
+++ app/src/routes/auth/login/+server.js
@@ -1,51 +1,91 @@
-import { back_api, back_api_origin } from '$lib/const.js';
-import moment from 'moment-timezone';
 import { sql_con } from "$lib/server/db";
+import { error, json } from '@sveltejs/kit';
+import { getQueryStr } from "$lib/server/lib";
+import jwt from 'jsonwebtoken';
+import { ACCESS_TOKEN_SECRET, REFRESH_TOKEN_SECRET } from '$env/static/private';
+import bcrypt from 'bcrypt';
+import moment from "moment-timezone";
+import axios from "axios";
+import { back_api } from "$lib/const.js";
 
-import axios from 'axios';
+/*
+>> 프론트에서 가져온 추가 정보들로 DB 입력 / JWT토큰 생성해서 아이디 값 저장 / 다시 DB에 토큰 값 저장하기!
+>> 이후 리턴 한 다음에 프론트에서 메인페이지로 refresh 하기!!
+*/
+export async function POST({ request, cookies }) {
+
+    const { id, password } = await request.json();
+
+    let userInfo = {}
+    try {
 
 
-
-
-
-// hooks.server.js 에서 받아온 유저 정보 +layout.js 로 넘기기 위한 중간 단계
-export const load = async ({ locals, request, cookies }) => {
-
-    const visitCookie = cookies.get('visit', { path: '/' })
-
-    // 쿠키 설정이 안되어 있으면 작업 GO!!
-    if (!visitCookie || visitCookie != 'true') {
-        console.log('기존 DB 없으면 insert! 있으면 update! 가짜 카운트랑 진짜 카운트!');
         try {
-            const res = await axios.get(`${back_api}/insert_n_update_count`)
+            const getUserRes = await axios.post(`${back_api}/auth/login_idchk`, {
+                id, password
+            })
+            userInfo = getUserRes.data.userInfo
+
         } catch (error) {
-
+            return json({ message: error.response.data.message }, { status: 400 })
         }
+        const isMatch = await bcrypt.compare(password, userInfo.password);
 
-        // 작업 후에는 쿠키 설정 해주기!
-        const now = new Date();
-        const midnight = new Date(now);
-        midnight.setHours(24, 0, 0, 0); // 자정 = 오늘 24:00:00.000
-        cookies.set('visit', 'true', {
-            expires: midnight,
-            httpOnly: true,
-            secure: true,
-            path: '/',
-        });
-    } else {
-        console.log('쿠키 설정 되어 있으면 fake_count 만 업데이트!!');
-        try {
-            const res = await axios.get(`${back_api}/update_count`)
-        } catch (error) {
+        if (isMatch) {
+            const accessPayload = {
+                userId: userInfo.idx,
+                rate: userInfo.rate
+            }
+
+            const refreshPayload = {
+                userId: userInfo.idx
+            }
+
+            const accessToken = jwt.sign(accessPayload, ACCESS_TOKEN_SECRET, { expiresIn: '15m' });
+            const refreshToken = jwt.sign(refreshPayload, REFRESH_TOKEN_SECRET, { expiresIn: '14d' });
+
+            // 리프레쉬 토큰 / 마지막 접속 시간 업데이트
+
+            try {
+                const res = await axios.post(`${back_api}/auth/token_update`, {
+                    refreshToken,
+                    idx: userInfo.idx
+                })
+            } catch (error) {
+                return json({ message: error.response.data.message }, { status: 400 })
+            }
+
+            cookies.set('access_token', accessToken, {
+                httpOnly: true,
+                secure: true,
+                path: '/',
+                // maxAge: 60 * 15
+                maxAge: 5
+            });
+
+            cookies.set('refresh_token', refreshToken, {
+                httpOnly: true,
+                secure: true,
+                path: '/',
+                maxAge: 60 * 60 * 24 * 14
+            });
+
+            console.log(cookies);
+            
+
+        } else {
+            return json({ message: '비밀번호가 일치하지 않습니다.' }, { status: 400 })
         }
+    } catch (error) {
+        console.error(error.message);
+
     }
-
-    console.log('+layout.server.js!!!');
-    console.log(locals.userInfo);
-
-
-    return {
-        // 이건 회원 정보 넘겨주기!!
-        user: locals.userInfo
-    }
+    return json({ userInfo })
 }
+
+
+// /*
+// 1. 아이디 비번 체크 후 액세스토큰 / 리프레쉬토큰 생성
+// 2. 다 같이 idx값 저장후 액세스토큰은 쿠키에 / 리프레쉬토큰은 DB에 저장
+// 3. payload 에서는 userId 값이 들어감 (DB의 idx 값임)
+// */
